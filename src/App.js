6900b9bb50b064784181068b299423f33b137567
mintcc342g <mintcc342g@gmail.com>
2022-01-20 00:48:59 +0900

    add a map, the hill of maria

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const App = () => (
   <div>
-    Hello, Webpack!
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
+import Phaser from 'phaser';
+import GridEngine from 'grid-engine';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TheHillOfMariaScene from './scenes/TheHillOfMariaScene';
 
+export const config = {
+  type: Phaser.AUTO,
+  title: 'Assault Lily Idle Game',
+  parent: 'game-content',
+  orientation: Phaser.Scale.LANDSCAPE,
+  localStorageName: 'Assault Lily Idle Game',
+  width: 630,
+  hight: 810,
+  autoRound: true,
+  pixelArt: true,
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.NONE,
+  },
+  scene: [
+    TheHillOfMariaScene,
+  ],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    }
+  },
+  plugins: {
+    scene: [
+      {
+        key: 'gridEngine',
+        plugin: GridEngine,
+        mapping: 'gridEngine',
+      },
+    ],
+  },
+};
+  
+const game = new Phaser.Game(config);
+  
 ReactDOM.render(
   <App />,
   document.getElementById('root') || document.createElement('div')
